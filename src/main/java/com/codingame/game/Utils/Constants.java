0800ce843b21a59a16784bbfbe8c9d4dea5412f0
Tzupy <tzupie@gmail.com>
2018-09-18 14:57:19 +0300

    Fixed a bug when the player could PUSH during a MOVE turn. If both players push the same line in opposite directions nothing will happen.

--- src/main/java/com/codingame/game/Utils/Constants.java
+++ src/main/java/com/codingame/game/Utils/Constants.java
@@ -93,5 +93,14 @@ public class Constants {
         public Vector2 asValue() {
             return value;
         }
+        public Direction getOpposite() {
+            switch(this) {
+                case UP: return DOWN;
+                case DOWN: return UP;
+                case LEFT: return RIGHT;
+                case RIGHT: return LEFT;
+            }
+            return null;
+        }
     }
 }
