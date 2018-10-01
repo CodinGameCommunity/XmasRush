a2086e0e0e48fc991aa3ed703511860fc90fbb98
Tzupy <tzupie@gmail.com>
2018-10-01 13:40:00 +0300

    Updated boss and statement for new MOVE syntax

--- config/Boss.java
+++ config/Boss.java
@@ -1,13 +1,42 @@
-class Player
-{
-    public static void main(String[] args)
-    {
-        System.out.println("PUSH 3 DOWN");
-        System.out.println("MOVE 1 UP");
-        System.out.println("PUSH 5 LEFT");
-        System.out.println("MOVE 1 DOWN");
-        System.out.println("PUSH 5 LEFT");
-        System.out.println("MOVE 1 RIGHT");
-        System.out.println("PUSH 5 LEFT");
+import java.util.Scanner;
+
+class Player {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int turn = 0;
+
+        while (true) {
+            for (int i = 0; i < 7; i++) {
+                String line = scanner.nextLine();
+                System.err.println(line);
+            }
+            int numPlayerCards = scanner.nextInt();
+            String questItemId = scanner.next();
+            int numOpponentCards = scanner.nextInt();
+            int playerX = scanner.nextInt();
+            int playerY = scanner.nextInt();
+            int opponentX = scanner.nextInt();
+            int opponentY = scanner.nextInt();
+            String playerTile = scanner.next();
+            String opponentTile = scanner.next();
+            if (scanner.hasNextLine()) {
+                scanner.nextLine();
+            }
+
+            // testing player input
+            System.err.println(numPlayerCards + " " + questItemId + " " + numOpponentCards);
+            System.err.println(playerX + " " + playerY + " " + opponentX + " " + opponentY);
+            System.err.println(playerTile + " " + opponentTile);
+
+            if (turn == 0) System.out.println("PUSH 1 LEFT");
+            if (turn == 1) System.out.println("MOVE UP");
+            if (turn == 2) System.out.println("PUSH 5 LEFT");
+            if (turn == 3) System.out.println("MOVE DOWN");
+            if (turn == 4) System.out.println("PUSH 5 LEFT");
+            if (turn == 5) System.out.println("MOVE RIGHT");
+            if (turn == 6) System.out.println("PUSH 5 LEFT");
+            turn++;
+        }
     }
 }
