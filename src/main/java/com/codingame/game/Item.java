a19e4fe73780738f83aee6b0dc230e901eb40af5
Tzupy <tzupie@gmail.com>
2018-10-04 11:36:00 +0300

    Documentation for Item

--- src/main/java/com/codingame/game/Item.java
+++ src/main/java/com/codingame/game/Item.java
@@ -1,25 +1,41 @@
 package com.codingame.game;
 
-import com.codingame.game.Utils.Vector2;
-
+/**
+ * Item is an immutable type representing the icon that can appear on a tile or card.
+ * Each item is uniquely identified by its name and id of the player it can be acquired by.
+ */
 public class Item {
+    /**
+     * All uppercase String object.
+     * May only be assigned values from Constants.ITEM_NAMES.
+     */
     private String name;
+
+    /**
+     * The id of the player that can acquire the item (0 for player or 1 for opponent).
+     */
     private int playerId;
 
+    /**
+     * Create an Item.
+     * @param name Item name. Must be a valid Item name.
+     * @param playerId The id of the player that can acquire the item (0 for player or 1 for opponent).
+     */
     public Item(String name, int playerId) {
         this.name = name;
         this.playerId = playerId;
     }
 
-    public Item(String name, int playerId, Vector2 pos) {
-        this.name = name;
-        this.playerId = playerId;
-    }
-
+    /**
+     * @return the item name.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @return the id of the player that can acquire the item (0 for player or 1 for opponent).
+     */
     public int getPlayerId() {
         return this.playerId;
     }
