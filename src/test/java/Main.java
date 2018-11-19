d2cb853d2907e8c6078b75373818186cfa3fe52b
Tzupy <tzupie@gmail.com>
2018-11-20 01:32:08 +0200

    Run 100 test matches

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,17 +1,28 @@
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.dto.GameResult;
 
 public class Main {
     public static void main(String[] args) {
 
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+        for (int i = 0; i < 100; i++) {
+            MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+
+            gameRunner.addAgent(PlayerAI.class);
+            gameRunner.addAgent(EmptyAI.class);
+
+            //required leagueLevel = 0
+            System.setProperty("league.level", "0");
+            gameRunner.setSeed(20L);
+            GameResult result = gameRunner.simulate();
+            System.out.println(String.format("Match %d: %d - %d", i, result.scores.get(0), result.scores.get(1)));
+        }
+        /*MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
         gameRunner.addAgent(PlayerAI.class);
         gameRunner.addAgent(EmptyAI.class);
 
-        //required leagueLevel = 0
         System.setProperty("league.level", "0");
-        //gameRunner.setSeed(20L);
-
-        gameRunner.start();
+        gameRunner.setSeed(20L);
+        gameRunner.start();*/
     }
 }
