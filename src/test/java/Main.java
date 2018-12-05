3ee1b635e2a8bcd1fed10b82d24678ba205cd7f7
Tzupy <tzupie@gmail.com>
2018-12-05 18:17:56 +0200

    Updated demo with player avatars

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,10 +3,13 @@ import com.codingame.gameengine.runner.MultiplayerGameRunner;
 public class Main {
     public static void main(String[] args) {
 
+        final String AVATAR_TZUPY = "https://static.codingame.com/servlet/fileservlet?id=3633797551022&format=profile_avatar";
+        final String AVATAR_RONICA = "https://static.codingame.com/servlet/fileservlet?id=23023322189851&format=profile_avatar";
+
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
-        gameRunner.addAgent(Player1.class);
-        gameRunner.addAgent(Player2.class);
+        gameRunner.addAgent(Player1.class, "Tzupy", AVATAR_TZUPY);
+        gameRunner.addAgent(Player2.class, "rnkey", AVATAR_RONICA);
 
         System.setProperty("league.level", "3");
         gameRunner.setSeed(20L);
