efcf8804732c76da9d99154d2211e734b8dc7bb6
Tzupy <tzupie@gmail.com>
2018-12-06 00:49:54 +0200

    Reverted to my original AI, no stealing

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,10 +4,10 @@ import com.codingame.gameengine.runner.dto.GameResult;
 public class Main {
     public static void main(String[] args) {
 
-        String league = "3";
+        String league = "2";
 
         runBatch(league, 1000);
-        //runOnce(league, 7577249279199645558L);
+        //runOnce(league, -4505592547209682872L);
     }
 
     private static void runBatch(String league, int numRounds) {
@@ -17,10 +17,10 @@ public class Main {
         for (int i = 0; i < numRounds; i++) {
             MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
-            //gameRunner.addAgent(PlayerAStar.class);
+            gameRunner.addAgent(PlayerAStar.class);
             //gameRunner.addAgent(EmptyAI.class);
             gameRunner.addAgent("python.exe config/level1/Boss.py3");
-            gameRunner.addAgent("python.exe config/level2/Boss.py3");
+            //gameRunner.addAgent("python.exe config/level2/Boss.py3");
 
             System.setProperty("league.level", league);
 
@@ -53,10 +53,10 @@ public class Main {
     private static void runOnce(String league, long seed) {
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
-        //gameRunner.addAgent(PlayerAStar.class);
+        gameRunner.addAgent(PlayerAStar.class);
         //gameRunner.addAgent(EmptyAI.class);
         gameRunner.addAgent("python.exe config/level1/Boss.py3");
-        gameRunner.addAgent("python.exe config/level2/Boss.py3");
+        //gameRunner.addAgent("python.exe config/level2/Boss.py3");
 
         System.setProperty("league.level", league);
         gameRunner.setSeed(seed);
